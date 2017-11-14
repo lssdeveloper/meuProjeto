05ae796219f8078b83dc08c766b248d1c48e0c23
Leandro Serra <leandro.serra.10@gmail.com>
2017-11-14 17:14:12 -0200

    Binding de propriedades customizadas com @Input

--- README.md
+++ README.md
@@ -3,4 +3,5 @@ Testando o Angular 4
 
 9.12. Exibindo condicionalmente com as diretivas ngIf e hidden
 9.13. Iterando com a diretiva ngFor
+9.14. Binding de propriedades customizadas com @Input
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { FuncionarioCardComponent } from './funcionario-card/funcionario-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FuncionarioCardComponent
   ],
   imports: [
     BrowserModule, 
